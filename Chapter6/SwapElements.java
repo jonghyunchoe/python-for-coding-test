59fd3853ddc3c1037cb1cad760b257064cc37de0
Jonghyun Choe <andy.choe729@gmail.com>
2021-01-10 21:33:57 +0900

    Update SwapElements.java

--- Chapter6/SwapElements.java
+++ Chapter6/SwapElements.java
@@ -1,5 +1,32 @@
-package Chapter6.java;
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class SwapTwoArrays {
+public class SwapElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] A = new int[N];
+        int[] B = new int[N];
+        for (int i=0; i<N; i++) 
+            A[i] = sc.nextInt();
+        for (int i=0; i<N; i++)
+            B[i] = sc.nextInt();
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i=0; i<K; i++) {
+            swap(A, B, i, N-i-1);
+        }
+        int sum = 0;
+        for (int e: A)
+            sum += e;   
+        System.out.println(sum);
+    }
     
+    private static void swap(int[] a, int[] b, int i, int j) {
+        int temp = a[i];
+        a[i] = b[j];
+        b[j] = temp;
+    }
 }
