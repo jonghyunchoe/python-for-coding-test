df6fb79c3e5b3aad2e83c8fff7bda2760b4c6b81
Jonghyun Choe <andy.choe729@gmail.com>
2021-01-10 21:22:37 +0900

    Week 2 Study

--- Chapter6/AscendingGrades.java
+++ Chapter6/AscendingGrades.java
@@ -1,5 +1,29 @@
-package Chapter6.java;
+import java.util.Scanner;
 
 public class AscendingGrades {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] names = new String[N];
+        int[] numbers = new int[N];
+        for (int i=0; i<N; i++) {
+            names[i] = sc.next();
+            numbers[i] = sc.nextInt();
+        }
+        for (int i=0; i<N; i++) {
+            int ith_number = numbers[i];
+            int index = i;
+            for (int j=1; j<N; j++) {
+                if (ith_number > numbers[j]) {
+                    ith_number = numbers[j];
+                    index = j;
+                }
+            }
+            String temp = names[i];
+            names[i] = names[index];
+            names[index] = temp;
+        }
+        for (String e: names)
+            System.out.print(e + " ");
+    }    
 }
--- Chapter6/UpDown.java
+++ Chapter6/UpDown.java
@@ -1,1 +1,17 @@
-va
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class UpDown {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Integer[] numbers = new Integer[N];
+        for (int i=0; i<N; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        Arrays.sort(numbers, Collections.reverseOrder());
+        for (int e: numbers)
+            System.out.print(e + " ");
+    }
+}
